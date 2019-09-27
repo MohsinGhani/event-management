e5982796d9f356b9fb749d309da1cdf299e945ad
aftabumer <innocentboy0300@gmail.com>
2019-09-27 13:06:56 +0500

    extra cards and fields remove

--- src/views/VenueDetail/Venues.js
+++ src/views/VenueDetail/Venues.js
@@ -44,135 +44,51 @@ class Venues extends Component {
               {/* <GlobleVariables venue={venue} /> */}
 
               <div className={classes.section + " typo_section"}>
-        <div className={classes.container}>
-          <div id="typography">
-            <div className={classes.typo + " typo"}>
-              <h2 className={classes.title + " typo-title"}>{venue.name}</h2>
-              <p style={{ display: "flex", alignItems: "center" }}>
-                <img
-                  src={call}
-                  alt="icon"
-                  width="30px"
-                  height="30px"
-                  style={{ paddingRight: "5px" }}
-                />
-                {venue.phone}
-              </p>
-              <p style={{ display: "flex", alignItems: "center" }}>
-                <img
-                  src={email}
-                  alt="icon"
-                  width="30px"
-                  height="30px"
-                  style={{ paddingRight: "5px" }}
-                />
-                {venue.email}
-              </p>
-              <p style={{ display: "flex", alignItems: "center" }}>
-                <img
-                  // src={require('path')}'
-                  className="text"
-                  src={address}
-                  alt="icon"
-                  width="30px"
-                  height="30px"
-                  style={{ paddingRight: "5px" }}
-                />
-                {venue.address}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
+                <div className={classes.container}>
+                  <div id="typography">
+                    <div className={classes.typo + " typo"}>
+                      <h2 className={classes.title + " typo-title"}>
+                        {venue.name}
+                      </h2>
+                      <p style={{ display: "flex", alignItems: "center" }}>
+                        <img
+                          src={call}
+                          alt="icon"
+                          width="30px"
+                          height="30px"
+                          style={{ paddingRight: "5px" }}
+                        />
+                        {venue.phone}
+                      </p>
+                      <p style={{ display: "flex", alignItems: "center" }}>
+                        <img
+                          src={email}
+                          alt="icon"
+                          width="30px"
+                          height="30px"
+                          style={{ paddingRight: "5px" }}
+                        />
+                        {venue.email}
+                      </p>
+                      <p style={{ display: "flex", alignItems: "center" }}>
+                        <img
+                          // src={require('path')}'
+                          className="text"
+                          src={address}
+                          alt="icon"
+                          width="30px"
+                          height="30px"
+                          style={{ paddingRight: "5px" }}
+                        />
+                        {venue.address}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </GridContainer>
           </GridItem>
           <GridItem xs={12} sm={9} md={9} lg={9}>
-            <GridContainer style={{ marginRight: 0 }}>
-              <Card>
-                <CardBody
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div>
-                    Guest Capicity
-                    <div style={{ borderTop: "3px solid black" }}>
-                      {/* {venue.capacity} */}
-                      Capicity
-                    </div>
-                  </div>
-                  <div>
-                    Venue Type
-                    <div style={{ borderTop: "3px solid black" }}>
-                      Operational Hours
-                    </div>
-                  </div>
-                  <div>
-                    Operational Hours
-                    <div style={{ borderTop: "3px solid black" }}>
-                      Oprational hour
-                    </div>
-                  </div>
-                  <div>
-                    Dimention
-                    <div style={{ borderTop: "3px solid black" }}>
-                      {/* {venue.lenght}
-                      <div>{venue.width}</div> */}
-                      length and width
-                    </div>
-                  </div>
-                </CardBody>
-              </Card>
-            </GridContainer>
-            {/* <GridContainer style={{ marginRight: 0 }}>
-              // <div className={classes.container}>
-              //   <div id="nav-tabs"> 
-
-              <CustomTabs
-                headerColor="danger"
-                tabs={[
-                  {
-                    tabName: "Event Types",
-                    tabIcon: Event,
-
-                    tabContent: (
-                      <Grid
-                        container
-                        direction="row"
-                        justify="flex-start"
-                        alignItems="flex-start"
-                      >
-                        {venue.serviesFacilities.map(event => (
-                          <GridItem
-                            xs={12}
-                            sm={12}
-                            md={4}
-                            lg={4}
-                            style={{
-                              padding: "0",
-                              width: "35%",
-                              overflow: "hidden",
-                              whiteSpace: "nowrap",
-                              textOverflow: "ellipsis",
-                              margin: "5px 0 5px 0"
-                            }}
-                          >
-                            <img
-                              src={checked}
-                              alt="icon"
-                              width="20px"
-                              height="20px"
-                              style={{ paddingRight: "5px" }}
-                            />
-                            <span title={event}>{event}</span>
-                          </GridItem>
-                        ))}
-                      </Grid>
-                    )
-                  }
-                ]}
-              />
-            </GridContainer> */}
-
             <GridContainer style={{ marginRight: 0 }}>
               {/* <div className={classes.container}>
                 <div id="nav-tabs"> */}
@@ -191,33 +107,48 @@ class Venues extends Component {
                         justify="flex-start"
                         alignItems="flex-start"
                       >
-                        {venue.serviesFacilities && venue.serviesFacilities.map(services => (
-                          <GridItem
-                            xs={12}
-                            sm={12}
-                            md={4}
-                            lg={4}
-                            style={{
-                              padding: "0",
-                              width: "35%",
-                              overflow: "hidden",
-                              whiteSpace: "nowrap",
-                              textOverflow: "ellipsis",
-                              margin: "5px 0 5px 0"
-                            }}
-                          >
-                            <img
-                              src={checked}
-                              alt="icon"
-                              width="20px"
-                              height="20px"
-                              style={{ paddingRight: "5px" }}
-                            />
-                            <span title={services.title}>{services.title}</span>
-                            <span title={services.price}>{services.price}</span>
+                        {venue.serviesFacilities &&
+                          venue.serviesFacilities.map(services => (
+                            // <GridItem
+                            //   xs={12}
+                            //   sm={12}
+                            //   md={4}
+                            //   lg={4}
+                            //   style={{
+                            //     padding: "0",
+                            //     width: "35%",
+                            //     overflow: "hidden",
+                            //     whiteSpace: "nowrap",
+                            //     textOverflow: "ellipsis",
+                            //     margin: "5px 0 5px 0"
+                            //   }}
+                            // >
+                            <GridItem
+                              xs={12}
+                              sm={12}
+                              md={4}
+                              lg={4}
+                              style={{
+                                display: "flex",
+                                justifyContent: "space-between"
+                              }}
+                            >
+                              <img
+                                src={checked}
+                                alt="icon"
+                                width="20px"
+                                height="20px"
+                                style={{ paddingRight: "5px" }}
+                              />
+                              <span title={services.title}>
+                                {services.title}
+                              </span>
 
-                          </GridItem>
-                        ))}
+                              <span title={services.price}>
+                                Rs: {services.price}
+                              </span>
+                            </GridItem>
+                          ))}
                       </Grid>
                     )
                   }
@@ -225,35 +156,6 @@ class Venues extends Component {
               />
             </GridContainer>
 
-            <GridContainer style={{ marginRight: 0 }}>
-              <CustomTabs
-                headerColor="danger"
-                tabs={[
-                  {
-                    tabName: "Pricing",
-                    tabIcon: Tag,
-
-                    tabContent: (
-                      <p className={classes.textCenter}>Rs: {venue.price}</p>
-                    )
-                  }
-                ]}
-              />
-            </GridContainer>
-            {/* <GridContainer style={{ marginRight: 0 }}>
-              <CustomTabs
-                headerColor="danger"
-                tabs={[
-                  {
-                    tabName: "Venues Packages",
-                    tabIcon: Package,
-                    tabContent: (
-                      <p className={classes.textCenter}>{venue.packages}</p>
-                    )
-                  }
-                ]}
-              />
-            </GridContainer> */}
             <GridContainer style={{ marginRight: 0 }}>
               <CustomTabs
                 headerColor="danger"
