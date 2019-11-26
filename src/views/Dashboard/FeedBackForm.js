30351106c2ab9ae051e810069eb86f45115dac0f
aftabumer <innocentboy0300@gmail.com>
2019-11-26 18:32:11 +0500

    -create feedback forms

--- src/views/Dashboard/FeedBackForm.js
+++ src/views/Dashboard/FeedBackForm.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-import IconButton from "@material-ui/core/IconButton";
+import { connect } from "react-redux";
+import { authAction, venueAction } from "./../../store/actions";
+
 import Dialog from "@material-ui/core/Dialog";
 import Slide from "@material-ui/core/Slide";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
-// @material-ui/icons
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import Close from "@material-ui/icons/Close";
-import DeleteIcon from "@material-ui/icons/Delete";
-
-import star from "../../../src/assets/icons/star.svg";
-
 // core components
-import Card from "components/Card/Card";
-import CardHeader from "dashboard-components/Card/CardHeader.jsx";
-import CardBody from "dashboard-components/Card/CardBody.jsx";
 import TextField from "@material-ui/core/TextField";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
 import ReactLoading from "react-loading";
 import Button from "components/CustomButtons/Button.jsx";
 import javascriptStyles from "assets/jss/material-kit-react/views/componentsSections/javascriptStyles.jsx";
 import basicsStyle from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.jsx";
-import typographyStyle from "assets/jss/material-kit-react/views/componentsSections/typographyStyle.jsx";
+import RatingSystem from "./RatingSystem";
 
 function Transition(props) {
   return <Slide direction="down" {...props} />;
@@ -36,7 +26,16 @@ class FeedBackForm extends Component {
     super(props);
 
     this.state = {
-      feedBackModal: false
+      feedBackModal: false,
+
+      overAllRating: 0,
+      recommendRating: 0,
+      organizedRating: 0,
+      friendlyRating: 0,
+      futureRating: 0,
+
+      privateSuggestion: "",
+      publicSuggestion: ""
     };
   }
 
@@ -52,9 +51,67 @@ class FeedBackForm extends Component {
     this.setState(reviewModal);
   };
 
+  handlerChangeOnRating = (event, identifier) => {
+    this.setState({
+      [identifier]: event
+    });
+  };
+
+  handlerChangeOnComment = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleOnSave = () => {
+    const { feedbackDetails, user, eventId } = this.props;
+    console.log("eventId: ", eventId)
+    const {
+      privateSuggestion,
+      publicSuggestion,
+
+      overAllRating,
+      recommendRating,
+      organizedRating,
+      friendlyRating,
+      futureRating
+    } = this.state;
+    let rating =
+      (overAllRating +
+        recommendRating +
+        organizedRating +
+        friendlyRating +
+        futureRating) /
+      5;
+    const newFeedback = {
+      overAllRating,
+      recommendRating,
+      organizedRating,
+      friendlyRating,
+      futureRating,
+      combineRating: rating,
+      privateSuggestion,
+      publicSuggestion,
+      userId: user && user.uid,
+      eventId
+    };
+    feedbackDetails(newFeedback);
+  };
+
   render() {
-    const { classes } = this.props;
-    const { feedBackModal } = this.state;
+    const { classes, user, eventId } = this.props;
+    const {
+      feedBackModal,
+
+      privateSuggestion,
+      publicSuggestion,
+
+      overAllRating,
+      recommendRating,
+      organizedRating,
+      friendlyRating,
+      futureRating
+    } = this.state;
     return (
       <div>
         <Button
@@ -64,19 +121,6 @@ class FeedBackForm extends Component {
           // disabled={isDetailsButtonDisable}
           onClick={() => this.handleClickOpen("feedBackModal")}
         >
-          {/* {saveVenueLoader ? (
-            <ReactLoading
-              type={"spin"}
-              color={"#ffff"}
-              height={"20px"}
-              width={"20px"}
-            />
-          ) : (
-            <>
-              <LibraryBooks className={classes.icon} />
-              "Submit"
-            </>
-          )} */}
           FeedBack
         </Button>
         <Dialog
@@ -96,11 +140,120 @@ class FeedBackForm extends Component {
             disableTypography
             className={classes.modalHeader}
           >
-            <div className={classes.typo}>
-              <h4>The Life of Material Kit</h4>
+            <div>
+              <h4 style={{ fontWeight: 500, fontSize: "xx-large" }}>
+                Feedback Form
+              </h4>
             </div>
           </DialogTitle>
+          <DialogContent
+            id="classic-modal-slide-description"
+            className={classes.modalBody}
+          >
+            <div style={{ fontWeight: 500, fontSize: "larger" }}>
+              We would love to hear your thoughts, concerns or problems with
+              anything so we can improve!
+            </div>
+            <div style={{ margin: "30px 0px" }}>
+              {" "}
+              <div>
+                <div style={{ fontWeight: 500 }}>
+                  How organized was the event?
+                </div>
+                <div>
+                  <RatingSystem
+                    rating={organizedRating}
+                    handleOnChange={this.handlerChangeOnRating}
+                    identifier={"organizedRating"}
+                  />
+                </div>
+              </div>
+              <div>
+                <div style={{ fontWeight: 500 }}>
+                  How likely are you to recommend this event to a friend?
+                </div>
+                <div>
+                  <RatingSystem
+                    rating={recommendRating}
+                    handleOnChange={this.handlerChangeOnRating}
+                    identifier={"recommendRating"}
+                  />
+                </div>
+              </div>
+              <div>
+                <div style={{ fontWeight: 500 }}>
+                  How likely are you to attend one of our future events?
+                </div>
+                <div>
+                  <RatingSystem
+                    rating={futureRating}
+                    handleOnChange={this.handlerChangeOnRating}
+                    identifier={"futureRating"}
+                  />
+                </div>
+              </div>
+              <div>
+                <div style={{ fontWeight: 500 }}>
+                  How friendly was the staff?
+                </div>
+                <div>
+                  <RatingSystem
+                    rating={friendlyRating}
+                    handleOnChange={this.handlerChangeOnRating}
+                    identifier={"friendlyRating"}
+                  />
+                </div>
+              </div>
+              <div>
+                <div style={{ fontWeight: 500 }}>
+                  Please rate your thoughts on the event as a whole.
+                </div>
+                <div>
+                  <RatingSystem
+                    rating={overAllRating}
+                    handleOnChange={this.handlerChangeOnRating}
+                    identifier={"overAllRating"}
+                  />
+                </div>
+              </div>
+            </div>
 
+            <div>
+              <div style={{ fontWeight: 500 }}>
+                What would you suggest as future growth opportunities for this
+                event? OR Are there any other comments or suggestions you'd like
+                to make?
+              </div>
+              <TextField
+                id="outlined-multiline-static"
+                className={classes.textField}
+                rows="4"
+                label="Private Suggestion"
+                type="text"
+                name="privateSuggestion"
+                value={privateSuggestion}
+                onChange={this.handlerChangeOnComment}
+                margin="dense"
+                variant="outlined"
+                multiline
+                fullWidth
+              />
+              <TextField
+                id="outlined-multiline-static"
+                className={classes.textField}
+                rows="4"
+                label="Public Suggestion"
+                type="text"
+                name="publicSuggestion"
+                value={publicSuggestion}
+                onChange={this.handlerChangeOnComment}
+                margin="dense"
+                variant="outlined"
+                multiline
+                fullWidth
+              />
+            </div>
+          </DialogContent>
           <DialogActions className={classes.modalFooter}>
             <Button
               color="danger"
@@ -110,7 +263,7 @@ class FeedBackForm extends Component {
               Cancel
             </Button>
             <Button
-              // onClick={() => goto("/admin/pending-venue-status")}
+              onClick={() => this.handleOnSave(eventId)}
               color="transparent"
               simple
             >
@@ -122,8 +275,33 @@ class FeedBackForm extends Component {
     );
   }
 }
-export default withStyles({
-  ...basicsStyle,
-  ...javascriptStyles,
-  ...typographyStyle
-})(FeedBackForm);
+const mapStateToProps = state => {
+  const {
+    authReducer: { user, isLoggedIn },
+    venueReducer: { venue, feedback, createFeedbackLoader, createFeedbackError }
+  } = state;
+  return {
+    user,
+    isLoggedIn,
+
+    venue,
+
+    feedback,
+    createFeedbackLoader,
+    createFeedbackError
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    feedbackDetails: payload => dispatch(venueAction.createFeedback(payload))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  withStyles({
+    ...basicsStyle,
+    ...javascriptStyles
+  })(FeedBackForm)
+);
