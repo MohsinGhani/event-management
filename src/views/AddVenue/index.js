18b87944c1369fac4218d113860848e2be533192
mohsinghani <mohsinghani.777@gmail.com>
2019-11-26 18:22:43 +0500

    -remove extra code

--- src/views/AddVenue/index.js
+++ src/views/AddVenue/index.js
@@ -30,9 +30,7 @@ const validEmailRegex = RegExp(
   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 );
 // const validateForm = errors => {
-//   debugger;
 //   let valid = true;
-//   debugger;
 //   Object.values(errors).forEach(val => val.length > 0 && (valid = false));
 //   return valid;
 // };
