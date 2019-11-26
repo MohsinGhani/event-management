59a40719c79961d5737b4bc60e57cd4aa790f7e9
aftabumer <innocentboy0300@gmail.com>
2019-11-26 18:31:58 +0500

    -create rating system

--- src/views/Dashboard/RatingSystem.js
+++ src/views/Dashboard/RatingSystem.js
@@ -2,35 +2,17 @@ import React, { Component } from "react";
 import StarRatings from "react-star-ratings";
 
 class RatingSystem extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      // rating: 0
-      rating: 5
-
-    };
-  }
-
-  changeRating = newRating => {
-    this.setState({
-      rating: newRating
-    });
-  };
-
   render() {
-    const { rating } = this.state;
+    const { rating, handleOnChange, identifier } = this.props;
     return (
       <StarRatings
-        rating={this.state.rating}
+        rating={rating}
         starRatedColor="rgb(221, 175, 55)"
         starEmptyColor="rgb(203, 211, 227)"
         starHoverColor="rgb(221, 175, 55)"
         starDimension="20px"
         starSpacing="3px"
-        changeRating={newRating => {
-          this.changeRating(newRating);
-        }}
+        changeRating={e => handleOnChange(e, identifier)}
         numberOfStars={5}
         name="rating"
       />
